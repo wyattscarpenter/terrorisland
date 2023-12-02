2c6dc3645c23267c35982837ed695fdef541b2a9
wyattscarpenter <wyattscarpenter@gmail.com>
2023-12-02 05:33:24 -0800

    add new mirror, effectively just deleting the '*.orig' files

---
+++ mirror/mirror.sh
@@ -0,0 +1,2 @@
+wget -erobots=off --mirror --convert-links --adjust-extension --page-requisites --convert-links http://www.terrorisland.net
+# Note that you canNOT just use terrorisland.net, you have to include the www. Or else maybe there's a setting in wget to deal with this, but I don't know it. Also, you have to ignore the robots.txt, because the webserve doesn't have one.
