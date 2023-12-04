496aeca2c38f4bde1828a4835c069e6bf33fb054
wyattscarpenter <wyattscarpenter@gmail.com>
2023-12-04 08:28:35 -0800

    mostly just change readmes

--- book/make_books.sh
+++ book/make_books.sh
@@ -1,5 +1,5 @@
 cp -r ../mirror/www.terrorisland.net/ site/ # This step might take about a minute (on a HDD; on an SSD it should be shorter), but it's important to make sure we don't mess up the mirror on accident.
-ebook-convert || sudo apt install calibre #here I try to install ebook-convert if you don't have it.
+ebook-convert || sudo apt install -y calibre #here I try to install ebook-convert if you don't have it.
 ebook-convert foreword.md -o foreword.md.html
 #pandoc foreword.md -o foreword.md.html
 cp cover.html cover.pdn.png foreword.md.html site/
